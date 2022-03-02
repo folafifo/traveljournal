33e2ac95179592ae2b85d65a57c0b1cbbfd17800
Fola <folarinfifo@gmail.com>
2022-03-02 12:50:18 +0000

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <img className="globe" src="./images/globe.png" alt="" />
+      <h3 className="nav-text">my travel journal.</h3>
+    </div>
+  );
+}
